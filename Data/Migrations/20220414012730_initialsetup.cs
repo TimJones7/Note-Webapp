565a1cc292ad351121e650ecfa29bbf92d341b67
Tim Jones <timothy.jones@gmx.us>
2022-04-13 22:42:03 -0400

    Add project files.

---
+++ Data/Migrations/20220414012730_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFirstWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteTitle = table.Column<string>(nullable: true),
+                    NoteBody = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string NoteTitle { get; set;}
+        public string NoteBody { get; set; }
+
+      
+        public Note()
+        {
+
+        }
+
+    }
+
+}
