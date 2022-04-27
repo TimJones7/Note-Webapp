e18bb801f1519bb556cf9c7121a0572cfece6e78
Tim Jones <timothy.jones@gmx.us>
2022-04-26 20:38:05 -0400

    Getting rid of un-needed pages and cleaning up a bit for a break

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,21 +3,4 @@
 
 // Write your JavaScript code.
 
-$(function() {
-    var PlaceHolderElement = $('#PlaceHolderHere');
-    $('button[data-toggle="ajax-modal"]').click(function (event) {
 
-        var url = $(this).data(url);
-        var decodedUrl = decodeURIComponent(url);
-
-        $.get(decodedUrl).done(function (data) {
-            PlaceHolderElement.html(data);
-            PlaceHolderElement.find('.modal').modal('show');
-        })
-    })
-})
-
-
-$(document).ready(function () {
-    $('#nav-tab').tabs();
-});
